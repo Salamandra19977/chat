e304cbdd057e75d0c0a31613c9bd76a04c8ccf99
Salamandra19977 <salamandra199720@gmail.com>
2025-04-29 20:18:26 +0300

    2.0

--- database.js
+++ database.js
@@ -1,51 +1,3 @@
-// const fs = require("fs")
-
-// const dbFile = "./chat.db"
-// const exists = fs.existsSync(dbFile)
-// const sqlite3 = require("sqlite3").verbose()
-// const dbWrapper = require("sqlite")
-
-// let db
-
-// dbWrapper
-//     .open({
-//         filename: dbFile,
-//         driver: sqlite3.Database
-//     })
-//     .then(async dBase => {
-//         db = dBase
-//         try {
-//             if (!exists) {
-//                 await db.run(
-//                     `CREATE TABLE user (
-//                         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-//                         login TEXT,
-//                         password TEXT
-//                     );`
-//                 )
-
-//                 await db.run(
-//                     `INSERT INTO user (login, password) VALUES 
-//                     ('admin', 'admin'),
-//                     ('JavaScript', '12345');`
-//                 )
-
-//                 await db.run(
-//                     `CREATE TABLE message (
-//                         msg_id INTEGER PRIMARY KEY AUTOINCREMENT,
-//                         content TEXT,
-//                         autor INTEGER,
-//                         FOREIGN KEY(autor) REFERENCES user(user_id)
-//                     );`
-//                 )
-//             } else {
-//                 console.log(await db.all("SELECT * from user"))
-//             }
-//         } catch (dbError) {
-//             console.error(dbError)
-//         }
-//     })
-
 const fs = require("fs")
 
 const dbFile = "./chat.db"
@@ -55,44 +7,54 @@ const dbWrapper = require("sqlite")
 
 let db
 
-dbWrapper 
+dbWrapper
     .open({
-    filename: dbFile,
-    driver: sqlite3.Database
+        filename: dbFile,
+        driver: sqlite3.Database
     })
-    .then(async dBase =>{
+    .then(async dBase => {
         db = dBase
-    try {
-        if(!exists) {
-            await db.run (
-                `CREATE TABLE user (
-                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                login TEXT,
-                password TEXT
-                );`
-              
-            )
-            await db.run(
+        try {
+            if (!exists) {
+                await db.run(
+                    `CREATE TABLE user (
+                        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        login TEXT,
+                        password TEXT
+                    );`
+                )
+
+                await db.run(
                     `INSERT INTO user (login, password) VALUES 
                     ('admin', 'admin'),
                     ('JavaScript', '12345');`
                 )
 
-            await db.run (
-                `CREATE TABLE message (
-                msg_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                content TEXT,
-                autor TEXT
-                FOREIGN KEY(autor) REFERENCES user(user_id)
-                );`
-              
-            )
-
-            
-        } else {
-            console.log(await db.all("SELECT * from user"))
+                await db.run(
+                    `CREATE TABLE message (
+                        msg_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        content TEXT,
+                        autor INTEGER,
+                        FOREIGN KEY(autor) REFERENCES user(user_id)
+                    );`
+                )
+            } else {
+                console.log(await db.all("SELECT * from user"))
+            }
+        } catch (dbError) {
+            console.error(dbError)
         }
-    } catch (dbError){
-        console.error(dbError)
-    }
     })
+module.exports = {
+    getMessages: async () => {
+        try {
+            return await db.all(
+                `SELECT msg_id, content, login, user_id from message
+                JOIN user ON message.autor = user.user_id
+                `
+            )
+        } catch(dbError) {
+            console.error(dbError)
+        }
+    },
+}
